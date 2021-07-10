fac3458706835f49ef272b8e7625c735ca43ad21
Samuel Thurlow <thurlow.webstuff@gmail.com>
2021-07-10 08:02:45 +0100

    ADD MATH QUESTION GENERATOR
    Added a math question generator to script.js and a couple of elements
    needed to index.html. Spits out simple addition questions and give a
    brief congratulations if answered correctly before giving another question.
    
    Also added my name to readme.me

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Mars Mission Gamejam | The Odin Project
 ## Team
-- [Steve? Sam? Seb? Thurlow](https://github.com/SWThurlow) (S W Thurlow) UTC+1
+- [Samuel Thurlow](https://github.com/SWThurlow) (S W Thurlow) UTC+1
 - [Amrin - I'm guessing that is your real name?](https://github.com/Coderamrin) UTC+6
 - [Rich Keyzor](https://github.com/Web-Dev-Rich) (freckle.coder) UTC+1
 - [highbouncinglover's real name here](https://github.com/highbouncinglover) (highbouncinglover) UTC+5:45
--- script.js
+++ script.js
@@ -1,2 +1,64 @@
-// Testing
-alert('JavaScript is working!');
+//Retrieving DOM elements.
+const gameArea = document.querySelector('.gameArea');
+const qBox = document.querySelector('.questionBox');
+
+/*Generating and displaying question - To be revisited
+when we have our space questions sorted out.*/
+//Generating random numbers, wont be needed for space questions.
+function getNum() {
+    return Math.floor(Math.random() * 10);
+}
+
+//Checking answers will need altering for space questions.
+function checkAnswer(num1, num2, answer) {
+    //A switch statment to check if multiple choice or input type
+    //question could be good.
+    if(!(Number(answer) > 0 && Number(answer) < 20)){
+        return;
+    } else {
+        if(num1 + num2 === Number(answer)) {
+            [...qBox.childNodes].forEach(child => qBox.removeChild(child));
+            const congratsMsg = document.createElement('p');
+            congratsMsg.innerHTML = "That's Right <br> Well Done!";
+            qBox.appendChild(congratsMsg);
+
+            setTimeout(() => {
+                qBox.removeChild(congratsMsg);
+                newQuestion();
+            }, 2000);
+        }
+    }
+}
+
+//Displaying generated numbers as a question. 
+//Will need changing for space questions.
+function displayQ(num1, num2) {
+    //Another switch statement to check question type?
+    const qDisplay = document.createElement('p');
+    qDisplay.textContent = `${num1} + ${num2} = `
+    qBox.appendChild(qDisplay);
+
+    const answerInput = document.createElement('input');
+    qBox.appendChild(answerInput);
+
+    const answerBtn = document.createElement('button');
+    answerBtn.textContent = 'Answer';
+    answerBtn.addEventListener('click', () => {
+        checkAnswer(num1, num2, answerInput.value);
+    });
+    qBox.appendChild(answerBtn);
+}
+
+//Bringing all the other functions together. 
+function newQuestion() {
+    const num1 = getNum();
+    const num2 = getNum();
+    displayQ(num1, num2);
+}
+
+newQuestion();
+/*End of section that should need revisiting once we have space questions. 
+It would be better to not have it all just as functions in the long run.
+I'm thinking that newQuestion should be an IFFE and that returns displayQ and
+checkAnswer. That way the set timeout for the congratsMsg could be moved 
+out of checkAnswer so that the game is less recursive.*/
