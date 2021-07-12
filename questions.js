554c6558f4f5281097b7a2f58338cc7ae6ad6cdd
Rich Keyzor <web_dev_rich@keyzor.net>
2021-07-12 08:55:05 +0100

    Correct spelling of 'explanation' It was doing my head in!

--- questions.js
+++ questions.js
@@ -4,7 +4,7 @@ const questions = [
         question: "How many more moons does Jupiter have than Earth and Mars together?",
         questionType: "input",
         expectedAnswer: "76",
-        explination: "Earth and Mars together have 3 moons. If we take 3 from 79 we should get 76."
+        explanation: "Earth and Mars together have 3 moons. If we take 3 from 79 we should get 76."
     },
     {
         info: "Our solar system has eight planets, they are,  in order: Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus and Neptune. The inner four planets are rocky planets and the outer four are gas giants.",
@@ -12,28 +12,28 @@ const questions = [
         questionType: "multiple",
         expectedAnswer: "Saturn",
         choices: ["Venus", "Uranus", "Earth", "Saturn"],
-        explination: "The gas planets are further from the sun than the four rocky planets. That means we are actually looking for the sixth planet from the sun. Which is Saturn."
+        explanation: "The gas planets are further from the sun than the four rocky planets. That means we are actually looking for the sixth planet from the sun. Which is Saturn."
     },
     {
         info: "Sound travels through the air by particles vibrating and colliding with other particles making them vibrate. Space is a vacuum, a vacuum is an area with nothing in it so the are no particles there, this means sound can not travel in space.",
         question: "Could an astronaut living on a space station hear a guitar if they were to play it?",
         questionType: "yes/no",
         expectedAnswer: "Yes",
-        explination: "A space station has air for an astronaut to live there this means that the sound can travel and they could hear themself playing the guitar. If they took the guitar outside the space station and played it they wouldn't be able to hear it in the vacuum of space."
+        explanation: "A space station has air for an astronaut to live there this means that the sound can travel and they could hear themself playing the guitar. If they took the guitar outside the space station and played it they wouldn't be able to hear it in the vacuum of space."
     },
     {
         info: "Our solar system has four gas planets, Jupiter, Saturn, Uranus and Neptune. They are further from the sun and are much bigger than the rocky inner planets, each of them also has many moons. They are called gas planets as they are balls of hydrogen and helium. ",
         question: "Could you stand on the surface of one of the gas planets?",
         questionType: "yes/no",
         expectedAnswer: "No",
-        explination: "Because gases aren't solid you would sink right through the surface of a gas planet. It would be a bit like trying to stand in the air."
+        explanation: "Because gases aren't solid you would sink right through the surface of a gas planet. It would be a bit like trying to stand in the air."
     },
     {
         info: "The Apollo 11 mission was the first time a human set foot on the moon, it was Neil Armstrong who took the first small step on the moon. It took Just over 4 days for them to reach the moon and they were there for just under a day, including a 7 hour sleep, before taking 3 days to return to earth.",
         question: "How many days did it take for the Apollo 11 mission to reach the moon, visit it's surface and return to Earth?",
         questionType: "input",
         expectedAnswer: "8",
-        explination: "We can count the time to reach the moon as 4 days and the time there as 1 day as was is slightly longer and the other is slightly shorter than a full day. This means to figure out how long it took we have to add 4, 1 and 3. 4 + 1 + 3 = 8"
+        explanation: "We can count the time to reach the moon as 4 days and the time there as 1 day as was is slightly longer and the other is slightly shorter than a full day. This means to figure out how long it took we have to add 4, 1 and 3. 4 + 1 + 3 = 8"
     }, 
     {
         info: "The Earth sits at a slight angle in its orbit around the sun. This tilt in the Earths axis means that different parts of the Earth receive the suns most direct rays. When the North Pole tilts towards the sun it is summer in the Northern Hemisphere and when the South Pole tilts towards the sun it is summer in the Southern Hemisphere.",
@@ -41,7 +41,7 @@ const questions = [
         questionType: "multiple",
         expectedAnswer: "The tilt in the Earth's axis.",
         choices: ["The tilt in the Earth's axis.", "The Earth getting closer and further from the sun throughout the year.", "Father Christmas"],
-        explination: "The tilt in the Earth’s axis means that at different times of year different parts of the earth get more direct sunlight. When a part of the Earth is getting more sunlight it gets warmer(spring/summer) and when a part of the Earth gets less sunlight it tends to be colder(autumn/winter)."
+        explanation: "The tilt in the Earth’s axis means that at different times of year different parts of the earth get more direct sunlight. When a part of the Earth is getting more sunlight it gets warmer(spring/summer) and when a part of the Earth gets less sunlight it tends to be colder(autumn/winter)."
     }
 ];
 
--- script.js
+++ script.js
@@ -1,121 +1,121 @@
-//Import questions array.
-import {questions} from'./questions.js';
+// Import questions array.
+import { questions } from './questions.js';
 
-//Retrieving DOM elements.
+// Retrieving DOM elements.
 const gameArea = document.querySelector('.gameArea');
 const qBox = document.querySelector('.questionBox');
 
-//To keep track of asked questions.
-let qsAsked = [];
+// To keep track of asked questions.
+const qsAsked = [];
 
-//Picking and displaying a random question.
-function pickQ(){
-    let selected = questions[Math.floor(Math.random() * questions.length)];
-    if(qsAsked.indexOf(selected) !== -1){
-        selected = pickQ();
-    } else {
-        qsAsked.push(selected);
-    } 
-    return selected;
+// Picking and displaying a random question.
+function pickQ() {
+  let selected = questions[Math.floor(Math.random() * questions.length)];
+  if (qsAsked.indexOf(selected) !== -1) {
+    selected = pickQ();
+  } else {
+    qsAsked.push(selected);
+  }
+  return selected;
 }
 
 function displayQ(question) {
-    const qInfo = document.createElement('p');
-    const asking = document.createElement('p');
-    function  displayInputQ() {
-        qInfo.textContent = question.info;
-        qBox.appendChild(qInfo);
+  const qInfo = document.createElement('p');
+  const asking = document.createElement('p');
+  function displayInputQ() {
+    qInfo.textContent = question.info;
+    qBox.appendChild(qInfo);
 
-        asking.textContent = question.question;
-        qBox.appendChild(asking);
+    asking.textContent = question.question;
+    qBox.appendChild(asking);
 
-        const answerInput = document.createElement('input');
-        answerInput.classList.add('answer');
-        qBox.appendChild(answerInput);
+    const answerInput = document.createElement('input');
+    answerInput.classList.add('answer');
+    qBox.appendChild(answerInput);
 
-        const answerBtn = document.createElement('button');
-        answerBtn.textContent = 'Answer';
-        answerBtn.addEventListener('click', () => {
-            marking(question, answerInput.value)
-        });
-        qBox.appendChild(answerBtn);
-    }
+    const answerBtn = document.createElement('button');
+    answerBtn.textContent = 'Answer';
+    answerBtn.addEventListener('click', () => {
+      marking(question, answerInput.value);
+    });
+    qBox.appendChild(answerBtn);
+  }
 
-    function displayMultipleQ() {
-        qInfo.textContent = question.info;
-        qBox.appendChild(qInfo);
+  function displayMultipleQ() {
+    qInfo.textContent = question.info;
+    qBox.appendChild(qInfo);
 
-        asking.textContent = question.question;
-        qBox.appendChild(asking);
+    asking.textContent = question.question;
+    qBox.appendChild(asking);
 
-        question.choices.forEach(answer => {
-            const answerBtn = document.createElement('button');
-            answerBtn.textContent = answer;
-            answerBtn.addEventListener('click', () => {
-                marking(question, answer);
-            });
-            qBox.appendChild(answerBtn);
-        });
-    }
+    question.choices.forEach(answer => {
+      const answerBtn = document.createElement('button');
+      answerBtn.textContent = answer;
+      answerBtn.addEventListener('click', () => {
+        marking(question, answer);
+      });
+      qBox.appendChild(answerBtn);
+    });
+  }
 
-    function displayYesNoQ() {
-        qInfo.textContent = question.info;
-        qBox.appendChild(qInfo);
+  function displayYesNoQ() {
+    qInfo.textContent = question.info;
+    qBox.appendChild(qInfo);
 
-        asking.textContent = question.question;
-        qBox.appendChild(asking);
+    asking.textContent = question.question;
+    qBox.appendChild(asking);
 
-        const yes = document.createElement('button');
-        yes.textContent = 'Yes';
-        yes.addEventListener('click', () => {
-            marking(question, 'Yes');
-        });
-        qBox.appendChild(yes);
+    const yes = document.createElement('button');
+    yes.textContent = 'Yes';
+    yes.addEventListener('click', () => {
+      marking(question, 'Yes');
+    });
+    qBox.appendChild(yes);
 
-        const no = document.createElement('button');
-        no.textContent = 'No';
-        no.addEventListener('click', () => {
-            marking(question, 'No');
-        });
-        qBox.appendChild(no);   
-    }
+    const no = document.createElement('button');
+    no.textContent = 'No';
+    no.addEventListener('click', () => {
+      marking(question, 'No');
+    });
+    qBox.appendChild(no);
+  }
 
-    switch(question.questionType) {
-        case 'input':
-            return displayInputQ();
-        case 'multiple':
-            return displayMultipleQ();
-        case 'yes/no':
-            return displayYesNoQ();
-    }
+  switch (question.questionType) {
+    case 'input':
+      return displayInputQ();
+    case 'multiple':
+      return displayMultipleQ();
+    case 'yes/no':
+      return displayYesNoQ();
+  }
+}
 
-};
-
-//Checking answers.
+// Checking answers.
 function marking(question, answer) {
-    let expected = question.expectedAnswer;
-    if(answer === expected) {
-        wellDone();
-    } else {
-        keepTrying(question);
-    }
+  const expected = question.expectedAnswer;
+  if (answer === expected) {
+    wellDone();
+  } else {
+    keepTrying(question);
+  }
 }
 
-//If answer is correct.
+// If answer is correct.
 function wellDone() {
-    const wellDoneMsg = document.createElement('p');
-    wellDoneMsg.textContent = "That's Correct! Well Done!";
-    qBox.appendChild(wellDoneMsg);
+  const wellDoneMsg = document.createElement('p');
+  wellDoneMsg.textContent = "That's Correct! Well Done!";
+  qBox.appendChild(wellDoneMsg);
 }
 
-//If answer is wrong. 
+// If answer is wrong.
 function keepTrying(question) {
-    const keepTrying = document.createElement('p');
-    keepTrying.textContent = "Unfortunately that's not right. Hopefully this explination can help you understand more.";
-    qBox.appendChild(keepTrying);
-    const explination = document.createElement('p');
-    explination.textContent= question.explination;
-    qBox.appendChild(explination);
+  const keepTrying = document.createElement('p');
+  keepTrying.textContent =
+    "Unfortunately that's not right. Hopefully this explanation can help you understand more.";
+  qBox.appendChild(keepTrying);
+  const explanation = document.createElement('p');
+  explanation.textContent = question.explanation;
+  qBox.appendChild(explanation);
 }
 
 displayQ(pickQ());
