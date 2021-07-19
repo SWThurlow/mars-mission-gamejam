c10d651f2b643bfc6886db4cfebd46cc5b6d7f76
Rich Keyzor <web_dev_rich@keyzor.net>
2021-07-19 18:23:31 +0100

    Initial commit of fuel meter

--- script.js
+++ script.js
@@ -4,85 +4,87 @@ import { questions } from './questions.js';
 // Retrieving DOM elements.
 const gameArea = document.querySelector('.gameArea');
 
-// To keep track of asked questions. 
-//In global scope to make it easier to use throughout game.
+// To keep track of asked questions.
+// In global scope to make it easier to use throughout game.
 const qsAsked = [];
 
 // Picking and displaying a random question.
-//Done as an IFFE as most of these fuunctions are just called by each other.
+// Done as an IFFE as most of these fuunctions are just called by each other.
 const questioning = (() => {
-    //Picking a random question and checking it hasn't already been picked.
-    function pickQ() {
+  // Picking a random question and checking it hasn't already been picked.
+  function pickQ() {
     let selected = questions[Math.floor(Math.random() * questions.length)];
     if (qsAsked.indexOf(selected) !== -1) {
-    selected = pickQ();
+      selected = pickQ();
     } else {
-    qsAsked.push(selected);
+      qsAsked.push(selected);
     }
     return selected;
   }
 
-  //Variables for displaying questions needed in multiple functions.
+  // Variables for displaying questions needed in multiple functions.
   const questionsBox = document.createElement('div');
   questionsBox.classList.add('questionBox');
-  const questionAndDetails = document.createElement("details");
-  questionAndDetails.classList.add("questionContainer");
+  const questionAndDetails = document.createElement('details');
+  questionAndDetails.classList.add('questionContainer');
   const qInfo = document.createElement('p');
-  qInfo.classList.add("qInfo");
-  const questionText = document.createElement('summary'); 
-  questionText.classList.add("questionText");
-    
+  qInfo.classList.add('qInfo');
+  const questionText = document.createElement('summary');
+  questionText.classList.add('questionText');
 
   function displayInputQ(question) {
-    const answerInput = document.createElement('input'); 
+    const answerInput = document.createElement('input');
     const answerBtn = document.createElement('button');
 
     qInfo.textContent = question.info;
     questionText.textContent = question.question;
 
     questionAndDetails.appendChild(questionText);
-    questionAndDetails.appendChild(qInfo);    
+    questionAndDetails.appendChild(qInfo);
 
     answerInput.classList.add('answer');
-    answerBtn.classList.add("submitAnswerBtn");
+    answerBtn.classList.add('submitAnswerBtn');
     answerBtn.textContent = 'Answer';
 
     answerBtn.addEventListener('click', () => {
-      [...questionsBox.childNodes].forEach(child => questionsBox.removeChild(child));
+      [...questionsBox.childNodes].forEach(child =>
+        questionsBox.removeChild(child)
+      );
       marking(question, answerInput.value);
-    });    
+    });
 
     questionsBox.appendChild(questionAndDetails);
-    questionsBox.appendChild(answerInput);    
+    questionsBox.appendChild(answerInput);
     questionsBox.appendChild(answerBtn);
   }
 
-  function displayMultipleQ(question) {      
+  function displayMultipleQ(question) {
     const answerBtnContainer = document.createElement('div');
 
     qInfo.textContent = question.info;
     questionText.textContent = question.question;
 
     questionAndDetails.appendChild(questionText);
-    questionAndDetails.appendChild(qInfo); 
+    questionAndDetails.appendChild(qInfo);
 
     questionsBox.appendChild(questionAndDetails);
 
     question.choices.forEach(answer => {
       const answerBtn = document.createElement('button');
-      answerBtn.classList.add("answerBtn");
-      answerBtnContainer.classList.add("answerBtnContainer");
+      answerBtn.classList.add('answerBtn');
+      answerBtnContainer.classList.add('answerBtnContainer');
 
       answerBtn.textContent = answer;
       answerBtn.addEventListener('click', () => {
-        [...questionsBox.childNodes].forEach(child => questionsBox.removeChild(child));
+        [...questionsBox.childNodes].forEach(child =>
+          questionsBox.removeChild(child)
+        );
         marking(question, answer);
       });
       answerBtnContainer.appendChild(answerBtn);
     });
 
     questionsBox.appendChild(answerBtnContainer);
-
   }
 
   function displayYesNoQ(question) {
@@ -95,18 +97,22 @@ const questioning = (() => {
 
     const yes = document.createElement('button');
     const no = document.createElement('button');
-    yes.classList.add("answerBtn");
-    no.classList.add("answerBtn");
+    yes.classList.add('answerBtn');
+    no.classList.add('answerBtn');
 
     yes.textContent = 'Yes';
     no.textContent = 'No';
     yes.addEventListener('click', () => {
-      [...questionsBox.childNodes].forEach(child => questionsBox.removeChild(child));
+      [...questionsBox.childNodes].forEach(child =>
+        questionsBox.removeChild(child)
+      );
       marking(question, 'Yes');
     });
-    
+
     no.addEventListener('click', () => {
-      [...questionsBox.childNodes].forEach(child => questionsBox.removeChild(child));
+      [...questionsBox.childNodes].forEach(child =>
+        questionsBox.removeChild(child)
+      );
       marking(question, 'No');
     });
 
@@ -114,9 +120,9 @@ const questioning = (() => {
     questionsBox.appendChild(no);
   }
 
-  //Switch statment to select the right function to display the question.
-  //Is wrapped in a statement to call pickQ() as a default argument and mean it can be called in the global scope (in the game function).
-  function displayQ(question = pickQ()) { 
+  // Switch statment to select the right function to display the question.
+  // Is wrapped in a statement to call pickQ() as a default argument and mean it can be called in the global scope (in the game function).
+  function displayQ(question = pickQ()) {
     gameArea.appendChild(questionsBox);
     switch (question.questionType) {
       case 'input':
@@ -128,6 +134,9 @@ const questioning = (() => {
       case 'yes/no':
         displayYesNoQ(question);
         break;
+      default:
+        alert('You hit the switch statement default case!');
+        break;
     }
   }
 
@@ -136,8 +145,10 @@ const questioning = (() => {
     const expected = question.expectedAnswer;
     if (answer === expected) {
       wellDone();
+      fuelControl.fuelInc();
     } else {
       keepTrying(question);
+      fuelControl.fuelDec();
     }
   }
 
@@ -148,9 +159,9 @@ const questioning = (() => {
     wellDoneMsg.textContent = "That's Correct! Well Done!";
     gameArea.appendChild(wellDoneMsg);
     const nextBtn = document.createElement('button');
-    nextBtn.textContent = 'Onwards!'
+    nextBtn.textContent = 'Onwards!';
     nextBtn.setAttribute('data-correct', 'true');
-    nextBtn.addEventListener('click', (e) => {
+    nextBtn.addEventListener('click', e => {
       winLose.winLose(e.target);
     });
     gameArea.appendChild(nextBtn);
@@ -161,66 +172,63 @@ const questioning = (() => {
     [...gameArea.childNodes].forEach(child => gameArea.removeChild(child));
     const keepTrying = document.createElement('p');
     keepTrying.textContent =
-    "Unfortunately that's not right. Hopefully this explanation can help you understand more.";
+      "Unfortunately that's not right. Hopefully this explanation can help you understand more.";
     gameArea.appendChild(keepTrying);
     const explanation = document.createElement('p');
     explanation.textContent = question.explanation;
     gameArea.appendChild(explanation);
     const nextBtn = document.createElement('button');
-    nextBtn.textContent = 'Onwards!'
+    nextBtn.textContent = 'Onwards!';
     nextBtn.setAttribute('data-correct', 'false');
-    nextBtn.addEventListener('click', (e) => {
+    nextBtn.addEventListener('click', e => {
       winLose.winLose(e.target);
     });
     gameArea.appendChild(nextBtn);
   }
-  return {displayQ}
+  return { displayQ };
 })();
 
-//Counting up correct/wrong answers wo check for win/loss conditions.
+// Counting up correct/wrong answers wo check for win/loss conditions.
 const winLose = (() => {
   let right = 0;
   let wrong = 0;
 
   function win() {
     const winMsg = document.createElement('p');
-    winMsg.textContent = 'Congratulations! You made it to Mars!'
+    winMsg.textContent = 'Congratulations! You made it to Mars!';
     gameArea.appendChild(winMsg);
   }
 
-  function loss() {
+  function lose() {
     const loseMsg = document.createElement('p');
     loseMsg.textContent = "Ohh NO! We've ran out of fuel! ";
     gameArea.appendChild(loseMsg);
   }
 
-  function winLose(target){
-    if(target.dataset.correct === 'true'){
+  function winLose(target) {
+    if (target.dataset.correct === 'true') {
       right++;
       [...gameArea.childNodes].forEach(child => gameArea.removeChild(child));
-      if(right === 2) {
+      if (right === 2) {
         win();
       } else {
-        questioning.displayQ()
+        questioning.displayQ();
       }
     } else {
       wrong++;
       [...gameArea.childNodes].forEach(child => gameArea.removeChild(child));
-      if(wrong === 2) {
-        loss();
+      if (wrong === 2) {
+        lose();
       } else {
-        questioning.displayQ()
+        questioning.displayQ();
       }
     }
   }
 
-  return {winLose}
+  return { winLose };
 })();
 
-
-
-
-//Start screen.
+// Start screen.
 const start = document.createElement('div');
 start.classList.add('start');
 const startH2 = document.createElement('h2');
@@ -237,16 +245,17 @@ startBtn.addEventListener('click', () => {
 });
 start.appendChild(startBtn);
 const helpBtn = document.createElement('button');
-helpBtn.textContent = 'Help';helpBtn.addEventListener('click', () => {
+helpBtn.textContent = 'Help';
+helpBtn.addEventListener('click', () => {
   gameArea.removeChild(start);
   gameArea.appendChild(help);
 });
 start.appendChild(helpBtn);
 
-//For on page load.
+// For on page load.
 gameArea.appendChild(start);
 
-//Help screen.
+// Help screen.
 const help = document.createElement('div');
 help.classList.add('help');
 const helpH2 = document.createElement('h2');
@@ -256,7 +265,8 @@ const helpH3 = document.createElement('h3');
 helpH3.textContent = 'How To Play The Game';
 help.appendChild(helpH3);
 const gameInstructions = document.createElement('p');
-gameInstructions.textContent = "You've been selected to be the captain of the first manned space mission to Mars! As the captain of the mission you will need to help the crew answer questions that will help power the ship and make sure you make it to mars in one piece."
+gameInstructions.textContent =
+  "You've been selected to be the captain of the first manned space mission to Mars! As the captain of the mission you will need to help the crew answer questions that will help power the ship and make sure you make it to mars in one piece.";
 help.appendChild(gameInstructions);
 const backBtn = document.createElement('button');
 backBtn.textContent = 'Close';
@@ -265,3 +275,38 @@ backBtn.addEventListener('click', () => {
   gameArea.appendChild(start);
 });
 help.appendChild(backBtn);
+
+/* =====================================================
+   FUEL METER
+   ===================================================== */
+
+const fuelControl = (() => {
+  // Set increment/decrement value for right/wrong answer
+  const _fuelMeterInc = 10;
+  const _fuelMeterDec = 15;
+
+  const fuelMeter = document.getElementById('fuel');
+
+  let _fuelMeterValue = fuelMeter.value;
+  const fuelMeterMin = fuelMeter.min;
+  const _fuelMeterMax = fuelMeter.max;
+
+  const fuelInc = () => {
+    if (_fuelMeterValue >= _fuelMeterMax) {
+      _fuelMeterValue = 100;
+    } else {
+      _fuelMeterValue += _fuelMeterInc;
+    }
+
+    fuelMeter.value = _fuelMeterValue;
+  };
+
+  const fuelDec = () => {
+    _fuelMeterValue -= _fuelMeterDec;
+    fuelMeter.value = _fuelMeterValue;
+  };
+
+  const getFuelValue = () => _fuelMeterValue;
+
+  return { fuelMeterMin, fuelInc, fuelDec, getFuelValue };
+})();
