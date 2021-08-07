d0b1d68cb6b2f750b0450f8c7683d8bc1df9d200
Samuel Thurlow <thurlow.webstuff@gmail.com>
2021-08-07 08:45:08 +0100

    ADDED QUESTIONS

--- questions.js
+++ questions.js
@@ -50,7 +50,7 @@ const questions = [
   },
   {
     info:
-      'The Earth sits at a slight angle in its orbit around the sun. This tilt in the Earths axis means that different parts of the Earth receive the suns most direct rays. When the North Pole tilts towards the sun, it is summer in the Northern Hemisphere and when the South Pole tilts towards the sun, it is summer in the Southern Hemisphere.',
+      "The Earth sits at a slight angle in its orbit around the sun. This tilt in the Earth's axis means that different parts of the Earth receive the suns most direct rays. When the North Pole tilts towards the sun, it is summer in the Northern Hemisphere and when the South Pole tilts towards the sun, it is summer in the Southern Hemisphere.",
     question: 'What is it that causes the different seasons?',
     questionType: 'multiple',
     expectedAnswer: "The tilt in the Earth's axis.",
@@ -64,7 +64,7 @@ const questions = [
   },
   {
     info:
-      'The asteroid belt is a donut-shaped region in the Solar System. It contains a great many solid, irregularly shaped bodies, of many sizes but much smaller than planets, called asteroids or minor planets. ',
+      'The asteroid belt is a donut-shaped region in the Solar System, between the fourth and fith planets. It contains a great many solid, irregularly shaped bodies, of many sizes but much smaller than planets, called asteroids or minor planets.',
     question: 'Where is the asteroid belt?',
     questionType: 'multiple',
     expectedAnswer: 'Between Mars and Jupiter',
@@ -99,6 +99,26 @@ const questions = [
     questionType: 'input',
     expectedAnswer: '2',
     explanation: 'Mars has 2 moons, Phobos and Deimos.',
+  },
+  {
+    info:
+      'The regolith, of a planet, is the blanket of loose rocks and dust that cover it. The regolith of Mars is has minerals rich in iron, which makes it look red.',
+    question: "What gives Mars it's colour?",
+    questionType: 'multiple',
+    expectedAnswer: 'The iron in its surface.',
+    choices: ['It has a red sky.', 'The iron in its surface.', 'It is secretly a tomato.'],
+    explanation:
+      "Iron can often look very red, like rust or how the iron in blood makes it red. As Mars has lots of iron rich minerals in it's surface this gives it the red colour that iron can often have.",
+  },
+  {
+    info:
+      'There a lots of things that can have objects in orbit around them. Planets can have moons orbit them, stars have planets, orbit them and black holes are often orbited by solar systems.',
+    question: 'What is the sun?',
+    questionType: 'multiple',
+    expectedAnswer: 'A star',
+    choices: ['A planet', 'An asteroid', 'A black hole', 'A star'],
+    explanation:
+      'The planets in our solar sytem orbit the sun. Because there are planets orbiting the sun we can tell that it is a star.',
   },
 ];
 
